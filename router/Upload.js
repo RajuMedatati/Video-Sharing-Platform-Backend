8c0917a236dee99ef0b8291c0aa09f7ba44edd3c
Rajesh mishra <rkm141297@gmail.com>
2023-03-28 07:11:40 +0530

    auth.js modified

--- router/Upload.js
+++ router/Upload.js
@@ -1,10 +1,15 @@
 const express = require("express");
 require("../db/conn.js");
+const User=require("../model/users.js");
+const auth=require("../middleware/auth.js")
 const Video = require("../model/Video.js");
 const router = express.Router();
 
-router.post('/upload', async (req, res) => {
-    const {userid,tittle,desc,imgUrl,videoUrl,category,visibility} = req.body;
+router.post('/upload', auth, async (req, res) => {
+
+    const userid = req.user._id
+
+    const {tittle,desc,imgUrl,videoUrl,category,visibility} = req.body;
     const data = new Video({
         userid:userid,
         tittle:tittle,
@@ -16,12 +21,15 @@ router.post('/upload', async (req, res) => {
     });
     try{
         const newVideo = await data.save();
-        res.json(newVideo)
+        res.status(200).send("Video uploaded successfully")
     }
     catch(e){
-        res.json({status:"failed",result:e.message})
+        res.status(404).send("error")
     }
 
 });
 
 module.exports = router;
+
+
+
